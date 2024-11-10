806f6de5778058394ecaae8e78a68adf9263d8c4
Jack Killelea <42438278+JackDKillelea@users.noreply.github.com>
2024-11-10 14:27:54 +0000

    Implemented database
    - Added a Flask SQL database to store the data the user inputs.
    
    - If the database is not created it will be created when the web page loads.
    
    - The user inputs on the Flask form are now stored in the database.
    
    - Email is the only unique field

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
+from datetime import datetime
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "myappkey1234"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+database = SQLAlchemy(app)
+
+class Form(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    first_name = database.Column(database.String(50), nullable=False)
+    last_name = database.Column(database.String(50), nullable=False)
+    email = database.Column(database.String(100), unique=True, nullable=False)
+    available_date = database.Column(database.Date, nullable=False)
+    current_status = database.Column(database.String(20), nullable=False)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -10,12 +24,16 @@ def index():
         email = request.form["email"]
         available_date = request.form["date"]
         current_status = request.form["currentStatus"]
-        print(first_name)
-        print(last_name)
-        print(email)
-        print(available_date)
-        print(current_status)
+        date_object = datetime.strptime(available_date, "%Y-%m-%d")
+
+        form = Form(first_name=first_name, last_name=last_name, email=email,
+                    available_date=date_object, current_status=current_status)
+        database.session.add(form)
+        database.session.commit()
 
     return render_template("index.html")
 
-app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    with app.app_context():  # Create a new session
+        database.create_all()  # Create the database tables if they don't exist'
+        app.run(debug=True, port=5001)
