2795fa4900ba32986200c574e050b3aafc12233c
Jack Killelea <42438278+JackDKillelea@users.noreply.github.com>
2024-11-10 13:24:00 +0000

    Implement fetching data
    - Implemented fetching data from my Flask web form using my Python back end.
    
    - Fixed a bug with the submit button, changing the type from button to submit (making it send a post request instead of no request).
    
    - Fixed an issue with the radio button selections returning "on" instead of the value.

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["firstName"]
+        last_name = request.form["lastName"]
+        email = request.form["email"]
+        available_date = request.form["date"]
+        current_status = request.form["currentStatus"]
+        print(first_name)
+        print(last_name)
+        print(email)
+        print(available_date)
+        print(current_status)
+
     return render_template("index.html")
 
 app.run(debug=True, port=5001)
